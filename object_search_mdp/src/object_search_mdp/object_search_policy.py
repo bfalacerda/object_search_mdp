1369e5c248ce7e49a4c559775aceca02ca2b8986
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2015-06-30 14:04:19 +0200

    more robust mdp state update

--- object_search_mdp/src/object_search_mdp/object_search_policy.py
+++ object_search_mdp/src/object_search_mdp/object_search_policy.py
@@ -7,7 +7,21 @@ from Queue import Queue
 
 class ObjectSearchPolicy(TopMapMdp):
     def __init__(self, states_file, labels_file, policy_file, n_waypoints, current_waypoint_val):
-        Mdp.__init__(self)
+        Mdp.__init__(self)               
+        ##list of attributes of an MDP object
+        #self.n_state_vars=0 #numer of variables used to defined the mdp state
+        #self.state_vars=[] #names of the different variables used to define the state
+        #self.state_vars_range={} #ranges for the state vars
+        #self.initial_state={} #dict indexed by the state vars
+        #self.n_props=0 #number of propositional labels
+        #self.props=[] #list of propositional label names
+        #self.props_def={} #dict of MdpPropDef instances. keys are the propositional labels names
+        #self.n_actions=0 #number of actions
+        #self.actions=[] #list of action names
+        #self.transitions=[] #list of MdpTransitionDef instances
+        #self.current_policy=[]
+        #self.reward_names={}
+        
         self.n_waypoints = n_waypoints
         self.n_da_states=0
         self.final_automaton_state=0
@@ -26,7 +40,15 @@ class ObjectSearchPolicy(TopMapMdp):
             if self.flat_states[state]['waypoint'] == waypoint_val:
                 self.current_state = state
                 return
-        print "ERROR GETTING NEXT STATE"
+        print "Jumped a state!"
+        new_state = dict(self.flat_states[self.current_state])
+        new_state['waypoint'] = waypoint_val
+        for i in range(0, len(self.flat_states)):
+            if self.flat_states[i] == new_state: 
+                self.current_state = i
+                print "Updated MDP state correctly"
+                return
+        print "Update mdp state error!"
     
     def get_current_nav_policy(self):
         sources = []
@@ -122,11 +144,7 @@ class ObjectSearchPolicy(TopMapMdp):
             if state == self.initial_state:
                 return flat_state
             flat_state = flat_state + 1
-            
-            
-        
-        
-        
+   
         
     def read_states(self, states_file, current_waypoint_val):
         f = open(states_file, 'r')
@@ -163,21 +181,6 @@ class ObjectSearchPolicy(TopMapMdp):
         self.state_vars_range['_da']=[0, self.n_da_states]
         f.close()
         
-        
-        
-        ##list of attributes of an MDP object
-        #self.n_state_vars=0 #numer of variables used to defined the mdp state
-        #self.state_vars=[] #names of the different variables used to define the state
-        #self.state_vars_range={} #ranges for the state vars
-        #self.initial_state={} #dict indexed by the state vars
-        #self.n_props=0 #number of propositional labels
-        #self.props=[] #list of propositional label names
-        #self.props_def={} #dict of MdpPropDef instances. keys are the propositional labels names
-        #self.n_actions=0 #number of actions
-        #self.actions=[] #list of action names
-        #self.transitions=[] #list of MdpTransitionDef instances
-        #self.current_policy=[]
-        #self.reward_names={}
         
     def get_object_var_name(self, object_name, waypoints_list, index):
         return object_name + '_at_' + waypoints_list[index]
